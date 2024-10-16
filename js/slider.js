e1cb2bd55978ea94382697bcd9c5ab7a5ab5cf96
unknown <juligan@gmail.com>
2024-10-16 02:20:46 -0300

    Se agregaron 3 al index

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,19 @@
-var slideImg = document.getElementById("slideImg");
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
 
-var images = new Array(
-    "images/img1.jpg",
-    "images/img2.jpg",
-    "images/img3.jpg"
-);
-var len = images.length;
-var i = 0;
-
-function slider(){
-    if(i > len-1 ){
-        i=0;
-    }
-    slideImg.src = images[i];
-    i++;
-    setTimeout('slider()', 3000);
+// Función para mostrar el slide actual
+function showSlide(slideIndex) {
+    const sliderWrapper = document.querySelector('.slider-wrapper');
+    sliderWrapper.style.transform = `translateX(-${slideIndex * 100}%)`;
 }
+
+// Función para iniciar el slider automático
+function startSlider() {
+    setInterval(() => {
+        currentSlide = (currentSlide + 1) % totalSlides; // Cambia al siguiente slide
+        showSlide(currentSlide);
+    }, 4000); // Cambia cada 4 segundos
+}
+
+window.onload = startSlider; // Inicia el slider al cargar la página
